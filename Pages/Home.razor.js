0810911fca67acdaa7c1e2915de3dbc239a421c9
platformaivn <platformaivn@users.noreply.github.com>
2024-09-20 04:40:24 +0000

    Upload wwwroot files 1.0.52

---
+++ Pages/Home.razor.js
@@ -0,0 +1,11 @@
+export function onLoad() {
+    // console.log('Loaded');
+}
+
+export function onUpdate() {
+    // console.log('Updated');
+}
+
+export function onDispose() {
+    // console.log('Disposed');
+}
